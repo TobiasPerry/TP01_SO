7e984af757b333e2517d552697ea48e58646a6ac
MarcosCasiraghi <mcasiraghi@itba.edu.ar>
2022-09-09 20:24:32 -0300

    emprolijacion de codigo

--- aplicacion.c
+++ aplicacion.c
@@ -57,54 +57,45 @@ int main(int argc, char* argv[]) {
 
 
     // int counter = argc;   //FORKS DE ESCLAVOS
-        const int slavecount = ceil((argc-1)*0.2);
-        int pid =1;
-        payload slaves[slavecount];
-        
-
-        int i;
-        for(i=0;i<slavecount;i++){
-            if (pipe(slaves[i].pipeOut) == -1) {
-                 perror("pipe");
-                 exit(-3);
-             }
-            if (pipe(slaves[i].pipeIn) == -1) {
+    const int slavecount = ceil((argc-1)*0.2);
+    int pid =1;
+    payload slaves[slavecount];
+    
+    int i;
+    for(i=0;i<slavecount;i++){
+        if (pipe(slaves[i].pipeOut) == -1) {
                 perror("pipe");
                 exit(-3);
             }
-                
+        if (pipe(slaves[i].pipeIn) == -1) {
+            perror("pipe");
+            exit(-3);
         }
-        for(i=0;i<slavecount && pid!=0;i++){
-            pid=fork();
-            if(pid==-1){
-                perror("forkerror");
-            }
-            else if(pid==0){
-                slaves[i].pid=getpid();
-            }
             
+    }
+    for(i=0;i<slavecount && pid!=0;i++){
+        pid=fork();
+        if(pid==-1){
+            perror("forkerror");
         }
-        i--;
-        //printf("%d\n",slaves[i].pid);
-        // pid = 260
-        // guarddo en el struct
+        else if(pid==0){
+            slaves[i].pid=getpid();
+        }
+        
+    }
+    i--;
+    
+    if (pid == 0) {  
+        //EL PROCESO ES ESCLAVO. DEBE ESTAR DESPIERTO HASTA QUE LA VARAIBLE QUEDAN_ARCHIVOS SEA 0.
+        int finished = 0;
+        
+        while(finished==0){
+            //ESPERO QUE ME PASEN UN ARCHIVO POR EL PIPE TODO: hacer este wait y mil mas(chequear todos juntos)
 
-       
+            char path[BUFSIZE];
+            read(slaves[i].pipeIn[0], path, BUFSIZE);
 
-
-        if (pid == 0) {  
-            //EL PROCESO ES ESCLAVO. DEBE ESTAR DESPIERTO HASTA QUE LA VARAIBLE QUEDAN_ARCHIVOS SEA 0.
-            int finished = 0;
-            
-            
-            while(finished==0){
-               
-             //ESPERO QUE ME PASEN UN ARCHIVO POR EL PIPE TODO: hacer este wait y mil mas(chequear todos juntos)
-
-                char path[BUFSIZE];
-                read(slaves[i].pipeIn[0], path, BUFSIZE);
-
-                if(strcmp(path,"0")!=0){
+            if(strcmp(path,"0")!=0){
                 char command[BUFSIZE] = "md5sum ";
                 strcat(command, path);
 
@@ -114,54 +105,45 @@ int main(int argc, char* argv[]) {
                 fgets(buf, BUFSIZE, md5);
                 buf[strlen(buf)-1]='\0';
                 
-              //TODO: que no imprima todo el path
+                //TODO: que no imprima todo el path
 
-            //TODO: HACER LOS DUP2
+                //TODO: HACER LOS DUP2
                 char pidbuf[10];
                 snprintf(pidbuf,10,"  %d\n", slaves[0].pid);
                 strcat(buf,pidbuf);
                 write(slaves[i].pipeOut[1], buf, strlen(buf));
-                }
-                
-                else{finished=1;}
-                
-            
+            }else{
+                finished=1;
+            }
         }
-            close(slaves[i].pipeIn[0]);
-            close(slaves[i].pipeOut[1]);
-            printf("llegue\n");
-            //SENDPOST();
-
-        } 
-        else {
-            int j;
-            
-            for(j=0;j<argc-1;j++){
-                write(slaves[j%slavecount].pipeIn[1],argv[j+1],BUFSIZE);
-            }
-            for(j=0;j<slavecount;j++){
-                 write(slaves[j].pipeIn[1],"0",BUFSIZE);
-                close(slaves[j].pipeIn[1]);
-            }
-            
-            char resultado[10];
-            read(slaves[0].pipeOut[0], resultado , BUFSIZE);
-            printf("%s\n",resultado);
-            
-            
-            //close de todos los slaves
+        close(slaves[i].pipeIn[0]);
+        close(slaves[i].pipeOut[1]);
+        printf("llegue\n");
+        //SENDPOST();
+    }else{
+        int j;
         
-      
+        for(j=0;j<argc-1;j++){
+            write(slaves[j%slavecount].pipeIn[1],argv[j+1],BUFSIZE);
         }
+        for(j=0;j<slavecount;j++){
+                write(slaves[j].pipeIn[1],"0",BUFSIZE);
+            close(slaves[j].pipeIn[1]);
+        }
+        
+        char resultado[10];
+        read(slaves[0].pipeOut[0], resultado , BUFSIZE);
+        printf("%s\n",resultado);
+        
+        
+        //close de todos los slaves
+    }
 
+// munmap(NULL, SHMSIZE);
+// shm_unlink(SHMDIR);
+// close(fd_app);
 
-
-
-    // munmap(NULL, SHMSIZE);
-    // shm_unlink(SHMDIR);
-    // close(fd_app);
-
-    return 0;
+return 0;
 
 }
 
